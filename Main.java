f4b6defa44b0ba4cb58e5d681ba9a91794e7ccd8
Mikhail-Kranyshev <91139884+Mikhail-Kranyshev@users.noreply.github.com>
2022-07-25 22:07:04 +0700

    Stage 2/8: Duck numbers

--- Main.java
+++ Main.java
@@ -3,6 +3,28 @@ package numbers;
 import java.util.Scanner;
 
 public class Main {
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean isOdd(int number) {
+        return number % 2 == 1;
+    }
+
+    public static boolean isBuzz(int number) {
+        return number % 7 == 0 || number % 10 == 7;
+    }
+
+    public static boolean isDuck(int number) {
+        while (number > 0) {
+            if (number % 10 == 0) {
+                return true;
+            }
+            number /= 10;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a natural number:");
@@ -10,28 +32,11 @@ public class Main {
         if (number < 1) {
             System.out.println("This number is not natural!");
         } else {
-            if (number % 2 == 1) {
-                System.out.println("This number is Odd.");
-            } else {
-                System.out.println("This number is Even.");
-            }
-            if (number % 7 == 0 || number % 10 == 7) {
-                System.out.println("""
-                        It is a Buzz number.
-                        Explanation:
-                        """);
-                if (number % 7 == 0 && number % 10 == 7) {
-                    System.out.println(number + " is divisible by 7 and ends with 7");
-                } else if (number % 7 == 0) {
-                    System.out.println(number + " is divisible by 7");
-                } else {
-                    System.out.println(number + " ends with 7");
-                }
-            } else {
-                System.out.printf("It is not a Buzz number.\n" +
-                        "Explanation:\n" +
-                        "%d is neither divisible by 7 nor does it end with 7.", number);
-            }
+            System.out.println("Properties of " + number);
+            System.out.println("        even: " + isEven(number));
+            System.out.println("         odd: " + isOdd(number));
+            System.out.println("        buzz: " + isBuzz(number));
+            System.out.println("        duck: " + isDuck(number));
         }
     }
 }
